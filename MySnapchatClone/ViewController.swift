f73b94a82202591e728e0220910288f665871744
ymyldz <113609896+ymyldz@users.noreply.github.com>
2022-12-25 17:50:53 +0300

    Initial Commit

---
+++ MySnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySnapchatClone
+//
+//  Created by Yusuf Mert Yıldız on 25.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
